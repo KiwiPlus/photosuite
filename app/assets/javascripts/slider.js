34110a1693c3bf6b482d8da65f62222444782562
kiwiplus <runecookie@gmail.com>
2014-11-19 11:18:32 +0100

    testing paintbrushjs

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,3 +1,45 @@
+window.onload=function(){
+
+
+var BlurIt = document.getElementById('blur');
+var SharpIt = document.getElementById('sharpen');
+var EdgeIt = document.getElementById('edges');
+var EmbossIt = document.getElementById('emboss');
+var GreysIt = document.getElementById('greyscale');
+var MosaicIt = document.getElementById('mosaic');
+var TintIt = document.getElementById('tint')
+
+//add event listener
+BlurIt.addEventListener('click', function(event) {
+document.getElementById('testpicture').setAttribute('class', 'filtered filter-blur pb-ref-filter-blur-0');
+});
+
+SharpIt.addEventListener('click', function(event) {
+document.getElementById('testpicture').setAttribute('class', 'filter-sharpen');
+});
+
+
+EdgeIt.addEventListener('click', function(event) {
+document.getElementById('testpicture').setAttribute('class', 'filter-edges');
+});
+
+
+EmbossIt.addEventListener('click', function(event) {
+document.getElementById('testpicture').setAttribute('class', 'filter-emboss');
+});
+
+
+GreysIt.addEventListener('click', function(event) {
+document.getElementById('testpicture').setAttribute('class', 'filter-greyscale');
+});
+
+
+TintIt.addEventListener('click', function(event) {
+document.getElementById('testpicture').setAttribute('class', 'filter-tint');
+});
+}
+
+
 
 
 //        var cam = Caman("#canvas", "assets/pic1.jpg");
